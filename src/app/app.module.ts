b31dfabb07089b271c95f06cd15c508d88307c56
ZOUHEIRBN <zouheir.b97@gmail.com>
2021-08-02 16:11:01 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ToolbarComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
