1a44a59f83d31943c31875ad82193b2ee2d8e2cf
ZOUHEIRBN <zouheir.b97@gmail.com>
2021-08-04 16:45:03 +0100

    Created section component

--- src/app/components/section/section.component.ts
+++ src/app/components/section/section.component.ts
@@ -9,9 +9,43 @@ export class SectionComponent implements OnInit {
   @Input() title = ''
   @Input() subtitle = ''
   @Input() divBg = ''
+  @Input() sectionStyle = ''
+  @Input() textStyle = ''
   constructor() { }
-
   ngOnInit(): void {
+    this.setColors()
+  }
+  setColors(): void {
+    if(this.sectionStyle === '' && this.divBg === ''){
+      this.sectionStyle = 'dark'
+    }
+    if(this.textStyle === ''){
+      this.textStyle = 'white'
+    }
+    let st = this.sectionStyle
+    if(st.endsWith('-dark')){
+      st = st.replace('-dark', '')
+    }
+    else if(st.endsWith('-light')){
+      st = st.replace('-light', '')
+    }
+
+    if(['primary', 'link', 'info', 'success', 'warning', 'danger', 'dark', 'text', 'white'].includes(st)){
+      this.sectionStyle = "has-background-"+this.sectionStyle
+    }
+
+
+    st = this.textStyle
+    if(st.endsWith('-dark')){
+      st = st.replace('-dark', '')
+    }
+    else if(st.endsWith('-light')){
+      st = st.replace('-light', '')
+    }
+
+    if(['primary', 'link', 'info', 'success', 'warning', 'danger', 'dark', 'text', 'white'].includes(st)){
+      this.textStyle = "has-text-"+this.textStyle
+    }
   }
 
 }
